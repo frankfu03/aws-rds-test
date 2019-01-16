fdadaba64e122d04c6fcf844284a5c09d1b09b8c
Frank Fu <frank.fu@ga.gov.au>
2019-01-16 11:16:17 +1100

    Enable running liquibase update on various environments (dev, test, prod)

--- build.sh
+++ build.sh
@@ -2,5 +2,31 @@
 
 set -e
 
+function usage {
+    echo "Usage: ./build.sh <env>
+        where
+        env
+            deployment environment, eg., dev, test, prod"
+    exit 1
+}
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        dev|test|prod )
+            env=$1
+            shift
+            ;;
+        * )
+            echo "Unknown option: $1"
+            usage
+            ;;
+    esac
+done
+
+if [ -z $env ]; then
+    echo "Error: deployment environment is empty."
+    usage
+fi
+
 # Build, test and verify all Java components
-mvn clean -s./maven-settings.xml install -DskipSystemTests
+mvn clean -s./maven-settings.xml install -P${env} -DskipSystemTests
